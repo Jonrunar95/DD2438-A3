4ec3b73cd03c68048f8e6a2f64e9eacab653b7c0
jonrunar <jonrunar@Jons-MacBook-Air.local>
2020-04-19 15:26:33 +0000

    goalie

--- Assets/Scrips/DroneAISoccer_blue.cs
+++ Assets/Scrips/DroneAISoccer_blue.cs
@@ -12,7 +12,6 @@ using Panda;
         private DroneController m_Drone; // the drone controller we want to use
         public GameObject terrain_manager_game_object;
         TerrainManager terrain_manager;
-		static SManager manager = new SManager(4f, 30f, 7f);
         public GameObject[] friends;
         public string friend_tag;
         public GameObject[] enemies;
@@ -25,34 +24,31 @@ using Panda;
 		public const float base_velocity = 8;
 		public List<Vector3> my_path;
 		PandaBehaviour myPandaBT;
+		bool GoalKeeper = false;
+		Vector3 oldBallPos;
+		Vector3 deltaBall = Vector3.zero;
+		Vector3 dir = Vector3.one;
         private void Start()
         {
-			Debug.Log("Start");
+			id = counter;
+    	    counter++;
+			if(id == 0) {
+				GoalKeeper = true;
+			}
+
 			myPandaBT = GetComponent<PandaBehaviour>();
-            // get the car controller
             m_Drone = GetComponent<DroneController>();
             terrain_manager = terrain_manager_game_object.GetComponent<TerrainManager>();
 
-            // note that both arrays will have holes when objects are destroyed
-            // but for initial planning they should work
-            friend_tag = "Blue";
+            friend_tag = gameObject.tag;
             if (friend_tag == "Blue")
                 enemy_tag = "Red";
             else
                 enemy_tag = "Blue";
-
             friends = GameObject.FindGameObjectsWithTag(friend_tag);
             enemies = GameObject.FindGameObjectsWithTag(enemy_tag);
-			Debug.Log("Friends: " + friends.Length + " Enemies: " + enemies.Length);
             ball = GameObject.FindGameObjectWithTag("Ball");
-
-			id = counter;
-			Debug.Log("Car ID: " + id);
-    	    counter++;
-        	System.Random rand = new System.Random();
-			manager.AddDrone(m_Drone, rand.Next(0, 100) < 90 ? 8f : base_velocity, other_goal.transform.position);
-			my_path = new List<Vector3>();
-			my_path.Add(other_goal.transform.position);
+			oldBallPos = ball.transform.position;
         }
 		[Task]
 		private bool BisInfrontBall()
@@ -76,34 +72,44 @@ using Panda;
 			Vector3 goal = ball.transform.position;
 			goal.x -= 15;
 			Vector3 move = (goal - m_Drone.transform.position).normalized;
-			Debug.DrawLine(transform.position, goal, Color.blue, 10f);
 			m_Drone.Move_vect(move);
 		}
 		[Task]
         private bool BisGoalie()
         {
-			Debug.Log(id+" Is Goalie?");
-			float minDist = float.MaxValue;
-			foreach(GameObject drone in friends) {
-				Debug.Log((drone.transform.position != m_Drone.transform.position) + " " + drone.transform.position + " " + m_Drone.transform.position);
-				if(drone.transform.position != m_Drone.transform.position) {
-					float dist = Vector3.Magnitude(drone.transform.position-own_goal.transform.position);
-					if(dist < minDist) {
-						minDist = dist;
-					}
-				}
-			}
-			if(Vector3.Magnitude(m_Drone.transform.position-own_goal.transform.position) < minDist) {
-				return true;
-			}
-			return false;
+			Debug.Log("is Goalie: " + GoalKeeper);
+			return GoalKeeper;
 		}
 		[Task]
 		private void BDefend(float p)
 		{
-			Debug.Log(id+" Defend " + p);
-			Vector3 move = (own_goal.transform.position - m_Drone.transform.position).normalized;
-			m_Drone.Move_vect(move);
+			bool b = false;
+			for(int t = 1; t < 101; t++) {
+				Vector3 ballPos = oldBallPos + t*deltaBall;
+				if(
+					ballPos.x > own_goal.transform.position.x-1 && ballPos.x < own_goal.transform.position.x+1 &&
+					ballPos.y > own_goal.transform.position.y && ballPos.y < own_goal.transform.position.y+10 &&
+					ballPos.z > own_goal.transform.position.z-15 && ballPos.z < own_goal.transform.position.z+15
+				) {
+					Vector3 move = (ballPos - m_Drone.transform.position).normalized;
+					m_Drone.Move_vect(move);
+					b = true;
+					break;
+				}
+			}
+			if(!b) {
+				Vector3 ballPos = own_goal.transform.position;
+				if(ball.transform.position.z < ballPos.z-15) {
+					ballPos.z -= 15;
+				} else if(ball.transform.position.z > ballPos.z+15) {
+					ballPos.z += 15;
+				} else {
+					ballPos.z = ball.transform.position.z;
+				}
+				
+				Vector3 move = (ballPos - m_Drone.transform.position).normalized;
+				m_Drone.Move_vect(move);
+			}
 		}
 		[Task]
 		private bool BisChaser()
@@ -139,56 +145,17 @@ using Panda;
 		[Task]
 		private void BDribble()
 		{
-			Debug.Log(id+" dribble?");
+			float dist = Mathf.Sqrt(Mathf.Pow(ball.transform.position.x-transform.position.x, 2) + Mathf.Pow(ball.transform.position.z-transform.position.z, 2));
+			Debug.Log("Distance: " + dist + " from: " + transform.position + " to: " + ball.transform.position);
 			Vector3 move = (ball.transform.position - m_Drone.transform.position).normalized;
 			m_Drone.Move_vect(move);
 		}
 		private void Update() {
+			Vector3 newBallPos = ball.transform.position;
+			deltaBall = newBallPos-oldBallPos;
+			oldBallPos = newBallPos;
 			myPandaBT.Reset();
 			myPandaBT.Tick();
 		}
-		 /*private void FixedUpdate()
-        {
-			
-			Vector3 move = manager.NextMove(m_Drone, ball.transform.position);
-
-			m_Drone.Move_vect(move);
-        	manager.Update(m_Drone);
-
-            // Execute your path here
-            // ...
-
-            Vector3 avg_pos = Vector3.zero;
-
-            foreach (GameObject friend in friends)
-            {
-                avg_pos += friend.transform.position;
-            }
-            avg_pos = avg_pos / friends.Length;
-            //Vector3 direction = (avg_pos - transform.position).normalized;
-            Vector3 direction = (ball.transform.position - transform.position).normalized;
-
-            
-
-            // this is how you access information about the terrain
-            int i = terrain_manager.myInfo.get_i_index(transform.position.x);
-            int j = terrain_manager.myInfo.get_j_index(transform.position.z);
-            float grid_center_x = terrain_manager.myInfo.get_x_pos(i);
-            float grid_center_z = terrain_manager.myInfo.get_z_pos(j);
-
-            Debug.DrawLine(transform.position, ball.transform.position, Color.black);
-            Debug.DrawLine(transform.position, own_goal.transform.position, Color.green);
-            Debug.DrawLine(transform.position, other_goal.transform.position, Color.yellow);
-            Debug.DrawLine(transform.position, friends[0].transform.position, Color.cyan);
-            Debug.DrawLine(transform.position, enemies[0].transform.position, Color.magenta);
-
-
-
-            // this is how you control the car
-            m_Drone.Move_vect(direction);
-            //m_Car.Move(0f, -1f, 1f, 0f);
-
-			
-        }*/
     }
 //}
--- Assets/Scrips/DroneAISoccer_red.cs
+++ Assets/Scrips/DroneAISoccer_red.cs
@@ -26,19 +26,19 @@ public class DroneAISoccer_red : MonoBehaviour
 	PandaBehaviour myPandaBT;
 	int id = 0;
     static int counter = 0;
+	bool GoalKeeper = false;
     private void Start()
     {
 		id = counter;
-		Debug.Log("Car ID: " + id);
     	counter++;
+		if(id == 0) {
+			GoalKeeper = true;
+		}
+		
 		myPandaBT = GetComponent<PandaBehaviour>();
-        // get the car controller
         m_Drone = GetComponent<DroneController>();
         terrain_manager = terrain_manager_game_object.GetComponent<TerrainManager>();
 
-
-        // note that both arrays will have holes when objects are destroyed
-        // but for initial planning they should work
         friend_tag = gameObject.tag;
         if (friend_tag == "Blue")
             enemy_tag = "Red";
@@ -47,12 +47,7 @@ public class DroneAISoccer_red : MonoBehaviour
 
         friends = GameObject.FindGameObjectsWithTag(friend_tag);
         enemies = GameObject.FindGameObjectsWithTag(enemy_tag);
-
         ball = GameObject.FindGameObjectWithTag("Ball");
-
-
-        // Plan your path here
-        // ...
     }
 	
 	[Task]
@@ -75,7 +70,7 @@ public class DroneAISoccer_red : MonoBehaviour
 	private void RGoBehind()
 	{
 		Vector3 goal = ball.transform.position;
-		goal.x -= 15;
+		goal.x += 15;
 		Vector3 move = (goal - m_Drone.transform.position).normalized;
 		Debug.DrawLine(transform.position, goal, Color.blue, 10f);
 		m_Drone.Move_vect(move);
@@ -83,21 +78,7 @@ public class DroneAISoccer_red : MonoBehaviour
 	[Task]
 	private bool RisGoalie()
 	{
-		Debug.Log(id+" Is Goalie?");
-		float minDist = float.MaxValue;
-		foreach(GameObject drone in friends) {
-			Debug.Log((drone.transform.position != m_Drone.transform.position) + " " + drone.transform.position + " " + m_Drone.transform.position);
-			if(drone.transform.position != m_Drone.transform.position) {
-				float dist = Vector3.Magnitude(drone.transform.position-own_goal.transform.position);
-				if(dist < minDist) {
-					minDist = dist;
-				}
-			}
-		}
-		if(Vector3.Magnitude(m_Drone.transform.position-own_goal.transform.position) < minDist) {
-			return true;
-		}
-		return false;
+		return GoalKeeper;
 	}
 	[Task]
 	private void RDefend(float p)
